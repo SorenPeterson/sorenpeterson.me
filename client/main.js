0bef87051ef7e668075ea64654d28752609bd9d3
Soren Peterson <soren468@gmail.com>
2015-06-18 21:58:48 -0700

    Shows login on marquee click

--- client/main.js
+++ client/main.js
@@ -10,3 +10,19 @@ Template.Portfolio.helpers({
 	}
 });
 
+Template.Layout.events({
+	'click marquee': function() {
+		var state;
+		Tracker.nonreactive(function() {
+			state = Session.get('showLogin');
+		});
+		Session.set('showLogin', !state);
+	}
+});
+
+Template.Layout.helpers({
+	showLogin: function() {
+		return Session.get('showLogin');
+	}
+});
+
