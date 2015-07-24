69f9a833919eb1ea6f70b764c14520b6a77f9c0c
Soren Peterson <soren468@gmail.com>
2015-07-24 17:59:54 -0500

    remove everything blog related

--- app/client/main.js
+++ app/client/main.js
@@ -1,53 +1,8 @@
 Meteor.subscribe('projects');
-Meteor.subscribe('videos');
-Meteor.subscribe('posts');
-
-var Helpers = {
-	isAdmin: function() {
-		return (Session.get('isAdmin') && Meteor.userId());
-	}
-}
 
 Template.Portfolio.helpers({
 	projects: function() {
 		return Projects.find();
 	}
 });
-
-Template.Header.events({
-	'click h1': function() {
-		var state;
-		Tracker.nonreactive(function() {
-			state = Session.get('showLogin');
-		});
-		Session.set('showLogin', !state);
-	}
-});
-
-Template.Layout.helpers({
-	showLogin: function() {
-		return Session.get('showLogin');
-	}
-});
-
-Template.Blog.helpers({
-	posts: function() {
-		return Posts.find({}, {sort: {_id: 1}, limit: 5});
-	}
-});
-Template.Blog.helpers(Helpers);
-
-Template.Blog.events({
-	'click button.save': function(e, tmpl) {
-		var _id = new Mongo.ObjectID(e.target.dataset.id);
-		var content = $(e.target.parentNode).find('pre').text();
-		console.log(_id);
-		console.log(Posts.findOne({_id: _id}));
-		Posts.update(_id, {
-			$set: {
-				content: content
-			}
-		});
-	}
-});	
 
--- app/client/routes.js
+++ app/client/routes.js
@@ -1,8 +1,5 @@
 Router.onBeforeAction(function() {
 	this.layout('Layout');
-	Meteor.call('isAdmin', function(err, res) {
-		Session.set('isAdmin', res);
-	});
 	this.next();
 })
 
@@ -15,24 +12,7 @@ Router.route('/portfolio', function() {
 	this.render('Portfolio');
 });
 
-Router.route('/videos', function() {
-	this.render('Videos');
-});
-
-Router.route('/videos/:category', function() {
-	this.render('VideoCategory', {
-		data: function() {
-			return {
-				category: this.params.category
-			};
-		}
-	});
-});
-
 Router.route('/opensource', function() {
 	this.render('OpenSource');
 });
 
-Router.route('/blog', function() {
-	this.render('Blog');
-});
