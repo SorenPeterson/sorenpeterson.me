cbfb7b84d7d31ade0c5667bbb780b89d1104ef0b
Soren Peterson <soren468@gmail.com>
2015-09-20 16:55:00 -0700

    the buttons are working and changing the screenshot containers correctly

--- app/client/main.js
+++ app/client/main.js
@@ -4,6 +4,24 @@ Template.PortfolioItem.events({
 	},
 	'click .portfolio-item-expanded .close': function(evt, tmpl) {
 		$(tmpl.find('.portfolio-item-expanded')).removeClass('open').addClass('closed');
+	},
+	'click .portfolio-item-expanded .directional-button.right': function(evt, tmpl) {
+		var $open_container = $(tmpl.find('.screenshot-container.open'));
+		$open_container.removeClass('open');
+		var $next = $open_container.next();
+		if(!$next[0]) {
+			$next = $open_container;
+		}
+		$next.addClass('open');
+	},
+	'click .portfolio-item-expanded .directional-button.left': function(evt, tmpl) {
+		var $open_container = $(tmpl.find('.screenshot-container.open'));
+		$open_container.removeClass('open');
+		var $prev = $open_container.prev();
+		if(!$prev[0]) {
+			$prev = $open_container;
+		}
+		$prev.addClass('open');
 	}
 });
 
